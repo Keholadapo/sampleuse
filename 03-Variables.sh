fdc4b296a18e2a0e2c41cce2ce7db3294a2302e0
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-06 19:34:29 -0400

    var

---
+++ 03-Variables.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# If we assign a name to some data, that name is called variable.
+
+#When it comes to data, we have different types, 1. Numbers. 2. Floating Values, 3. Characters. 4. Strings. 5. Booleans
+
+a=10
+b=xyz
+c=true
+d=9.999999
+
+# Shell by default does not support data types, everything is a string.
+
+# Access variable : $(a) or $a
+
+echo $a
