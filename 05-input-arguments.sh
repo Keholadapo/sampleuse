87bb75054b238d0e0ab2346a9cdec27c23f41fb1
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-08 19:29:23 -0400

    arg

--- 05-input-arguments.sh
+++ 05-input-arguments.sh
@@ -3,3 +3,9 @@
 # Special variable with regards to input, $0-$n, $*, $@, $#
 # Script name = $0
 echo $0
+echo First argument = $1
+echo Second Argument = $2
+## Beyond 9, From 10 argument ${}
+
+echo All arguments = $*
+echo All argument = $@
