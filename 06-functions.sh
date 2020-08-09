cdbc68d4443f26ea756a904c6cf905e134a3f306
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-08 20:55:15 -0400

    call

--- 06-functions.sh
+++ 06-functions.sh
@@ -5,6 +5,16 @@
 # Declare function
 
 sample() {
- echo Hello World from function
+  echo Hello World from function
+  echo a = $a
+
 }
 
+# Call the function
+
+# Main Program
+a=10
+sample
+
+# Observations
+# 1. Functions are always before main program
