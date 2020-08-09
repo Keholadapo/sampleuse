c267ca726b6ca804fb26cd19b34ff8ab2d170592
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-08 21:01:12 -0400

    func

--- 06-functions.sh
+++ 06-functions.sh
@@ -5,9 +5,10 @@
 # Declare function
 
 sample() {
+  a=30
   echo Hello World from function
-  echo a = $a
-
+  echo a in Function = $a
+  b=20
 }
 
 # Call the function
@@ -15,6 +16,9 @@ sample() {
 # Main Program
 a=10
 sample
+echo a in Main Program = $a
+echo b in Main Program = $b
 
 # Observations
 # 1. Functions are always before main program
+# 2. Variables in main programs can be assessed in function and vice-versa
