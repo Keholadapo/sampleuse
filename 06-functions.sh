7c0f31f774aca12de8aa5f2bc6fb77f32b495387
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-08 20:11:34 -0400

    function

---
+++ 06-functions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Variable holds data, Function holds commands. When you call function, it execute all the commands declare in function
+
+# Declare function
+
+sample90 {
+    echo Hello World from Function
+}
+
+# Call the function
