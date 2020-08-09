b1820cf650efbbdf50f4c75d400be9001e5413f1
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-08 22:59:17 -0400

    prj

--- project.sh
+++ project.sh
@@ -1,13 +1,100 @@
 #!/bin/bash
 
-yum install nginx -y
-curl -s -L -o /tmp/frontend.zip "https://dev.azure.com/DevOps-Batches/98e5c57f-66c8-4828-acd6-66158ed6ee33/_apis/git/repositories/65042ce1-fdc2-4472-9aa2-3ae9b87c1ee4/items?path=%2F&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=master&resolveLfs=true&%24format=zip&api-version=5.0&download=true"
-cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv static/* .
-rm -rf static README.md
-mv localhost.conf /etc/nginx/nginx.conf
+#Frontend
+#Mongodb
+#Redis
+#Mysql
+#Rabbitmq
+#Cart
+#Catalogue
+#Shipping
+#Payment
+#User
+#All
 
-systemctl enable nginx
-systemctl start nginx
+FRONTEND() {
+  echo "Installing Frontend Service"
+}
+
+MONGODB() {
+  echo "Installing MongoDD Service"
+}
+
+MYSQL() {
+  echo "Installing MySQL Service"
+}
+
+RABBITMQ() {
+  echo "Installing RabbitMQ Service"
+}
+
+REDIS() {
+  echo "Installing REDIS Service"
+}
+
+CART() {
+  echo "Installing Cart Service"
+}
+
+CATALOGUE() {
+  echo "Installing Catalogue Service"
+}
+
+USER() {
+  echo "Installing User Service"
+}
+
+SHIPPING() {
+  echo "Installing Shipping Service"
+}
+
+PAYMENT() {
+  echo "Installing Payment Service"
+}
+
+case $1 in
+  frontend)
+    frontend
+    ;;
+  mongodb)
+    mongodb
+    ;;
+  mysql)
+    mysql
+    ;;
+  rabbitmq)
+    rabbitmq
+    ;;
+  redis)
+    redis
+    ;;
+  cart)
+    cart
+    ;;
+  catalogue)
+    catalogue
+    ;;
+  user)
+    user
+    ;;
+  shipping)
+    shipping
+    ;;
+  payment)
+    payment
+    ;;
+  all)
+    frontend
+    mongodb
+    mysql
+    rabbitmq
+    redis
+    cart
+    catalogue
+    user
+    Shipping
+    payment
+    ;;
+esac
+
+  
