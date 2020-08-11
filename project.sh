0c13e0426f1eca926e2803e31cec9fb4f6e7afa3
Mariam Adedeji <Mariam@MacBook-Pro.local>
2020-08-10 20:14:00 -0400

    Mysql

--- project.sh
+++ project.sh
@@ -81,11 +81,11 @@ MYSQL() {
  Head "Installing MySQL Service"
  yum list installed | grep mysql-community-server &>/dev/null
  case $? in
-   0)
-     true
-     ;;
-   *)
-   curl -L -o /tmp/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar >>$LOG_FILE
+  0)
+   true
+   ;;
+  *)
+ curl -L -o /tmp/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar >>$LOG_FILE
  Stat $? "Download MYSQL Bundle\t"
  cd /tmp
  tar -xf mysql-5.7.28-1.el7.x86_64.rpm-bundle.tar
@@ -95,7 +95,7 @@ MYSQL() {
  yum install mysql-community-client-5.7.28-1.el7.x86_64.rpm mysql-community-common-5.7.28-1.el7.x86_64.rpm mysql-community-libs-5.7.28-1.el7.x86_64.rpm mysql-community-server-5.7.28-1.el7.x86_64.rpm -y &>>$LOG_FILE
  Stat $? "Install MYSQL Database\t"
  ;;
-esac
+ esac
 
  systemctl enable mysqld &>>$LOG_FILE
  systemctl start mysqld &>>$LOG_FILE
